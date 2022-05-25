7eee40b72b9c59f277732149ca0aade5641c0749
Fritz Gnad <github@feezenfreezen.de>
2022-05-25 16:00:02 +0200

    update mainnet v7

--- src/scripts/Config/script_config.js
+++ src/scripts/Config/script_config.js
@@ -5,12 +5,12 @@ var provider = null;
 var firstAccount;
 
 //Contract used in script details
-let contractAddress = "0xf7cd7a8B996c595b174544B0b6FCdeE9eDe88e81"; // << Rinkeby // Mainnet 25.5.2022: 0xF7E139CB289247366d8b039d84775A750cE51488
+let contractAddress = "0xF7E139CB289247366d8b039d84775A750cE51488"; // // Mainnet 25.5.2022: 0xF7E139CB289247366d8b039d84775A750cE51488
 //dependent contract used in script
 const dependentcontractAddress = "0x73AF8134139E12f23BAFa4505Bc59EbAaA621B93";
 //alchemcy api
 let alchemy_api =
-  "wss://eth-rinkeby.alchemyapi.io/v2/ESx7lVzbks2ViNPdQrNHwmZkdB3wydux"; // Mainnet: wss://eth-mainnet.alchemyapi.io/v2/_k2rn6sEaO0HBMO6huvwfSK-Q52KKYrK
+  "wss://eth-mainnet.alchemyapi.io/v2/_k2rn6sEaO0HBMO6huvwfSK-Q52KKYrK"; // Mainnet: wss://eth-mainnet.alchemyapi.io/v2/_k2rn6sEaO0HBMO6huvwfSK-Q52KKYrK
 const INFURA_KEY = "5b3b303e5c124bdfb7029389b1a0d599";
 
 //prices to be used for minting
