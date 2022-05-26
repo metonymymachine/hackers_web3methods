17d54d832fb64cad515d7f7626c24867c5a8f571
Fritz Gnad <github@feezenfreezen.de>
2022-05-26 20:04:26 +0200

    public sale update

--- src/scripts/web3/script.js
+++ src/scripts/web3/script.js
@@ -314,7 +314,7 @@ const addressStatus = async (acc) => {
     localStorage.setItem("cyclops_allowed", amount_allowed_cl);
   } else {
     $(".allow_list_text").text(
-      `You are not on the allowlist for our pre-sale. The public sale starts on May 26th, 6pm GMT.`
+      `You can mint up to 4 WHC per Transaction..`
     );
   }
 };
